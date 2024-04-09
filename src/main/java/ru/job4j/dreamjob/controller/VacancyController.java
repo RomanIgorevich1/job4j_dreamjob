8a3b1f800f97afbbc90d4afba43b412c1f339786
votmo <votmoyapochta1@bk.ru>
2024-04-09 22:32:55 +0300

    Создание вакансии

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -3,16 +3,20 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.SearchRepository;
 import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
+import ru.job4j.dreamjob.repository.VacancyRepository;
 
 @Controller
 @RequestMapping("/vacancies")
 public class VacancyController {
 
     private final SearchRepository<Vacancy> search = MemoryVacancyRepository.getInstance();
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
 
     @GetMapping
     public String getAll(Model model) {
@@ -23,5 +27,11 @@ public class VacancyController {
     @GetMapping("/create")
     public String getCreationPage() {
         return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
