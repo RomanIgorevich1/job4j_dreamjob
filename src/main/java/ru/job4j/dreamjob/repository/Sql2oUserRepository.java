68098b35c81b49a7ba60a10bdbb1bd07e5275bad
votmo <votmoyapochta1@bk.ru>
2024-05-15 14:17:26 +0300

    Многопоточность в базе данных

---
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -0,0 +1,50 @@
+package ru.job4j.dreamjob.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.Optional;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sql2oUserRepository.class.getName());
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                    INSERT INTO users(email, name, password)
+                    VALUES (:email, :name, :password)
+                    """;
+           var query = connection.createQuery(sql, true)
+                    .addParameter("email", user.getEmail())
+                    .addParameter("name", user.getName())
+                    .addParameter("password", user.getPassword());
+            int generateId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generateId);
+        } catch (Exception exception) {
+            LOGGER.error("\"Пользователь с такой почтой уже существует\"");
+        }
+        return Optional.of(user);
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery(
+                    "SELECT * FROM users WHERE email = :email AND password = :password");
+            var user = query.addParameter("email", email)
+                    .addParameter("password", password).executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/dreamjob/repository/Sql2oUserRepositoryTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.dreamjob.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+
+import ru.job4j.dreamjob.model.User;
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.util.Properties;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Sql2oUserRepositoryTest {
+    private static Sql2oUserRepository sql2oUserRepository;
+
+    private static Sql2o sql2o;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        Properties properties = new Properties();
+        try (InputStream inputStream = Sql2oUserRepository.class.getClassLoader()
+                .getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        String url = properties.getProperty("datasource.url");
+        String username = properties.getProperty("datasource.username");
+        String password = properties.getProperty("datasource.password");
+        DatasourceConfiguration configuration = new DatasourceConfiguration();
+        DataSource dataSource = configuration.connectionPool(url, username, password);
+        sql2o = configuration.databaseClient(dataSource);
+        sql2oUserRepository = new Sql2oUserRepository(sql2o);
+    }
+
+    @AfterEach
+    public void deleteFile() {
+        try (var connection = sql2o.open()) {
+            connection.createQuery("DELETE FROM users").executeUpdate();
+        }
+
+    }
+
+    @Test
+    public void whenSaveThenGetSame() {
+        User user1 = sql2oUserRepository.save(
+                new User(0, "email1", "name1", "password1")).get();
+        User savedUser = sql2oUserRepository.findByEmailAndPassword(user1.getEmail(), user1.getPassword()).get();
+        assertThat(savedUser).isEqualTo(user1);
+    }
+
+    @Test
+    public void whenSaveSeveralThenGetSame() {
+        User user1 = sql2oUserRepository.save(
+                new User(0, "email1", "name1", "password1")).get();
+        User user2 = sql2oUserRepository.save(
+                new User(0, "email2", "name2", "password2")).get();
+        User user3 = sql2oUserRepository.save(
+                new User(0, "email3", "name3", "password3")).get();
+        User savedUser = sql2oUserRepository.findByEmailAndPassword(user2.getEmail(), user2.getPassword()).get();
+        assertThat(savedUser).usingRecursiveComparison().isEqualTo(user2);
+
+    }
+
+    @Test
+    public void whenSameEmailThenErr() {
+        User user1 = sql2oUserRepository.save(
+                new User(0, "email1", "name1", "password1")).get();
+        User user2 = sql2oUserRepository.save(
+                new User(0, "email1", "name2", "password2")).get();
+        assertThat(sql2oUserRepository.findByEmailAndPassword(user1.getEmail(), user1.getPassword())).isNotEmpty();
+        assertThat(sql2oUserRepository.findByEmailAndPassword(user2.getEmail(), user2.getPassword())).isEmpty();
+    }
+
+}
